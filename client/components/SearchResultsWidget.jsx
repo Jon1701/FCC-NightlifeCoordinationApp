6478348ae66470499614368536ee09f154fb38a9
Jon Balon <development@jonbalon.com>
2016-12-30 17:41:06 -0500

    Implemented search results widget

---
+++ client/components/SearchResultsWidget.jsx
@@ -0,0 +1,47 @@
+// React dependencies.
+import React from 'react';  // React library.
+
+// React components.
+import MediaObject from 'components/MediaObject';
+
+// Redux dependencies.
+import { connect } from 'react-redux';  // Connect to redux store.
+
+// Component definition.
+class SearchResultsWidget extends React.Component {
+
+  // Component render.
+  render() {
+    // Return hidden element if no search results are provided.
+    if (!this.props.results) {
+      return (
+        <div className="hidden" />
+      );
+    }
+
+    // For each business, generate HTML with each field.
+    const businesses = this.props.results.businesses.map((val, idx) => (
+      <MediaObject key={idx} field={val} />
+    ));
+
+    // Display results.
+    return (
+      <div>{businesses}</div>
+    );
+  }
+}
+
+// Maps state to props.
+const mapStateToProps = state => (
+  { results: state.results }
+);
+
+// Allow component access to Redux store.
+export default connect(mapStateToProps, null)(SearchResultsWidget);
+
+// Typecheck this.props.
+SearchResultsWidget.propTypes = {
+  results: React.PropTypes.shape({
+    businesses: React.PropTypes.array,
+  }),
+};
