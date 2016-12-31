ece6aed1bd4f18e190e8dbc4154a038e374cb4b7
Jon Balon <development@jonbalon.com>
2016-12-30 23:43:49 -0500

    Added signup error and success messages

--- server/messages/index.js
+++ server/messages/index.js
@@ -8,10 +8,24 @@ const errors = {
     NO_TERM: response('NO_TERM', 'No search term provided.'),
     API_ERROR: response('API_ERROR', 'An error occurred while accessing the Yelp API.'),
   },
+  SIGNUP: {
+    MISSING_CREDENTIALS: response('MISSING_CREDENTIALS', 'Both a username and password are required.'),
+    INVALID_USERNAME: response('INVALID_USERNAME', 'Invalid username. Valid characters are letters, numbers, and underscore. Must be between 8 and 25 characters long.'),
+    INVALID_PASSWORD: response('INVALID_PASSWORD', 'Invalid password. Valid characters are letters, numbers, and underscore. Must be between 8 and 50 characters long.'),
+    PASSWORD_MISMATCH: response('PASSWORD_MISMATCH', 'Passwords do not match.'),
+    MISSING_PASSWORD_CONFIRMATION: response('MISSING_PASSWORD_CONFIRMATION', 'Both password and password confirmation are required.'),
+    USER_EXISTS: response('USER_EXISTS', 'User already exists.'),
+  },
+  DB: {
+    DB_ERROR: response('DB_ERROR', 'Unknown database error occurred.'),
+  },
 };
 
 // Success messages.
 const successes = {
+  SIGNUP: {
+    USER_CREATED: response('USER_CREATED', 'User successfully created.'),
+  },
   YELP: {
     API_SUCCESS: response('API_SUCCESS', 'Data successfully retrieved via Yelp API.'),
   },
