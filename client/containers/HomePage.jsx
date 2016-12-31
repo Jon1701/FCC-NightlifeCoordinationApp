97c74011c2c0380661bc26ab96960b4003f9db6d
Jon Balon <development@jonbalon.com>
2016-12-31 18:27:07 -0500

    Change welcome screen

--- client/containers/HomePage.jsx
+++ client/containers/HomePage.jsx
@@ -34,11 +34,7 @@ class HomePage extends React.Component {
         <div className="column is-10 is-offset-1">
 
           <div className="box">
-
-            <h1 className="title">Nightlife Coordination</h1>
-
-            <h5 className="subtitle is-5">A FreeCodeCamp project</h5>
-
+            <h4 className="subtitle is-4">Where are you going tonight?</h4>
           </div>
 
           <div className="box">
