6157252d1a537b6e65a1cf2d253aac8fc77e567c
Jon Balon <development@jonbalon.com>
2016-12-30 19:48:27 -0500

    Added functionality to store/read results in session storage

--- client/containers/HomePage.jsx
+++ client/containers/HomePage.jsx
@@ -3,16 +3,32 @@ import React from 'react';  // Standard React library.
 
 // Redux dependencies.
 import { connect } from 'react-redux';  // Connect to redux store.
+import { bindActionCreators } from 'redux'; // Bind action creators.
 
 // React components.
 import SearchWidget from 'components/SearchWidget';
 import SearchResultsWidget from 'components/SearchResultsWidget';
 
+// Actions.
+import { storeResults, deleteResults } from 'actions/index';  // Redux actions.
+
 // Component definition.
 class HomePage extends React.Component {
 
+  // Component Lifecycle Method.
+  componentDidMount() {
+    // Read search results from session storage.
+    const results = JSON.parse(sessionStorage.getItem('results'));
+
+    // If search results were found, store it in redux state.
+    if (results) {
+      this.props.storeResults(results);
+    }
+  }
+
   // Component render.
   render() {
+
     return (
       <div className="columns has-text-centered">
         <div className="column is-10 is-offset-1">
@@ -44,5 +60,10 @@ const mapStateToProps = state => (
   }
 );
 
+// Allow access of dispatch actions as props.
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({ storeResults, deleteResults }, dispatch)
+);
+
 // Allow component access to Redux store.
-export default connect(mapStateToProps, null)(HomePage);
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- client/reducers/reducer_results.js
+++ client/reducers/reducer_results.js
@@ -3,6 +3,15 @@ const results = (state = null, action) => {
   switch (action.type) {
     // Store action payload into state.
     case 'STORE_RESULTS':
+
+      // Store results in session storage.
+      try {
+        sessionStorage.setItem('results', JSON.stringify(action.payload));
+      } catch (e) {
+        // Do nothing.
+      }
+
+      // Return results in reducer.
       return action.payload;
 
     // Delete existing results from state.
